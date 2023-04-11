ca823a7a32fbe973368ab03fa856fa426d05c1c4
Franco Martin <main.francomartin@gmail.com>
2023-04-11 01:11:25 -0300

    Fix utf8 simbol

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -16,12 +16,12 @@ const Footer = (props: Props) => {
       <Logo />
       <Navigation />
       <SimpleGrid gap={8} columns={{ base: 1, md: 2 }}>
-        <Stack color={'textLight'} spacing={4}>
+        <Stack color={"textLight"} spacing={4}>
           <Text>
             Audiophile is an all in one stop to fulfill your audio needs. We're
             a small team of music lovers and sound specialists who are devoted
             to helping you get the most out of personal audio. Come and visit
-            our demo facility - we’re open 7 days a week.
+            our demo facility - we're open 7 days a week.
           </Text>
           <Text>Copyright 2021. All Rights Reserved</Text>
         </Stack>
