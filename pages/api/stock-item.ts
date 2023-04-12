6b0a03f3a7f75beb2293b8703defcbd5a32f220c
Franco Martin <main.francomartin@gmail.com>
2023-04-12 12:45:58 -0300

    test Add enviroment vercel

--- pages/api/stock-item.ts
+++ pages/api/stock-item.ts
@@ -2,33 +2,25 @@ import { NextApiRequest, NextApiResponse } from "next";
 import data from "../../constant/data.json";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  //   res.setHeader("Cache-Control", "s-maxage=86400");
-  if (req.method === "GET") {
-    const { id, category } = req.query;
-    if (!category && !id) {
-      return res.status(405).json({ message: "Method not allowed" });
-    }
-
-    let filteredData = data;
-
-    if (category) {
-      filteredData = filteredData.filter(
-        (element) => element.category === category
-      );
-      if (filteredData.length === 0) {
-        return res.status(404).json({ message: "Category not found" });
-      }
-    }
-
-    if (id) {
-      filteredData = filteredData.filter(
-        (element) => element.id === Number(id)
-      );
-      if (filteredData.length === 0) {
-        return res.status(404).json({ message: "Product not found" });
-      }
-    }
-
-    return res.status(200).json(filteredData);
+  if (req.method !== "GET") {
+    return res.status(405).json({ message: "Method not allowed" });
   }
+
+  const { id, category } = req.query;
+  if (!category && !id) {
+    return res.status(400).json({ message: "Missing parameters" });
+  }
+
+  const filteredData = data.filter((item) => {
+    return (
+      (!category || item.category === category) &&
+      (!id || item.id === Number(id))
+    );
+  });
+
+  if (filteredData.length === 0) {
+    return res.status(404).json({ message: "No items found" });
+  }
+
+  return res.status(200).json(filteredData);
 }
