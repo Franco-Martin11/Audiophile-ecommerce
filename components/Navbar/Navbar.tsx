3423fbc8eafb059fca2a4cfdb9714b4678fc7571
Franco Martin <main.francomartin@gmail.com>
2023-04-14 20:35:21 -0300

    Add - path and anchor tag in logo

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,12 +1,15 @@
 import { HStack } from "@chakra-ui/react";
 import { Cart, HamburgerMenu, Logo } from "./components";
+import Link from "next/link";
 type Props = {};
 
 const Navbar = (props: Props) => {
   return (
     <HStack as="nav" justifyContent={"space-between"} px="4" py="4">
       <HamburgerMenu />
-      <Logo />
+      <Link href={"/"} passHref>
+        <Logo />
+      </Link>
       {/* <Navigation /> */}
       <Cart />
     </HStack>
